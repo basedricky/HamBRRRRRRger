3dc802b49d5f45fb4f2633625246bae5608cce1e
Ricky Panh <ricky@dragonflyut.com>
2021-04-18 09:00:07 -0600

    get route

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,4 +1,23 @@
+
 const express = require('express');
-const burger = require('./burger.js');
+const handlebars = require('express-handlebars');
+const router = express.Router();
+const app = express();
+const burger = require('../models/burgers.js');
+
+
+app.engine('handlebars', handlebars({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+// get route
+
+router.get('/', (req, res) => {
+    burger.selectAll((data) => {
+        const handlebarBurger = {
+            burgers: data,
+        };
+        res.render('index', handlebarBurger);
+    })
+})
 
 module.exports = router
