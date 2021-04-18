bf550c5b860c11518f4953bebc48afece3a5ebbc
Ricky Panh <ricky@dragonflyut.com>
2021-04-18 09:58:32 -0600

    added burgers model

--- models/burger.js
+++ models/burger.js
@@ -1,1 +1,20 @@
 const orm = require('../config/orm.js');
+
+// the cats app activity was a life saver here
+
+const burgers = {
+    selectAll(cb) {
+        orm.selectAll('burgers', (res) => cb(res));
+    },
+
+    insertOne(reqBody, cb) {
+        orm.insertOne('burgers', reqBody, (res) => cb(res));
+    },
+
+    updateOne(id, cb) {
+        orm.updateOne(id, (res) => cb(res));
+    }
+
+}
+
+module.exports = burgers;
