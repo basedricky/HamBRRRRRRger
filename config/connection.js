8be0754cba2fcef34430bd5e9de5d239716ab9b5
Ricky Panh <ricky@dragonflyut.com>
2021-04-18 22:23:14 -0600

    first deploy to heroku

--- config/connection.js
+++ config/connection.js
@@ -9,7 +9,7 @@ if (process.env.JAWSDB_URL) {
         host: 'localhost',
         port: 3306,
         user: 'root',
-        password: '',
+        password: 'Topanga_801',
         database: 'burgers_db',
     });
 }
